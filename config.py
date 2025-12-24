3853969643fea845736a4ef0d00e8bf3df74d8de
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-12-25 02:10:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class Config(object):
     START_PIC   = os.environ.get("START_PIC", "https://files.catbox.moe/7ymnlp.jpg")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '8226767954').split()]
     FORCE_SUB   = os.environ.get("FORCE_SUB", "AnimeNexusNetwork") 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002734211536"))
+    LOG_CHANNEL = -1002456565415
     
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
